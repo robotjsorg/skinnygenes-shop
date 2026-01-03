7da1465b9943fe1575cb001c59d968f0ecb1bc42
jmcmahon443 <jmcmahon443@gmail.com>
2026-01-03 18:04:06 -0500

    empty string base path

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ command }) => {
   return {
     plugins: [react()],
-    base: command === 'serve' ? '/' : '/skinnygenes-shop/',
+    base: command === 'serve' ? '/' : '',
     appType: 'spa', // Explicitly declare as SPA
     build: {
       outDir: 'dist', // Output directory for the build
