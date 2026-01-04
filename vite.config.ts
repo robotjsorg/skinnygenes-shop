cc43b9cfeeae2ec8e7e219955f565e480fd76e64
jmcmahon443 <jmcmahon443@gmail.com>
2026-01-03 21:06:25 -0500

    try removing base path

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ command }) => {
   return {
     plugins: [react()],
-    base: '/skinnygenes-shop/',
+    base: '/', // /skinnygenes-shop/
     appType: 'spa',
     build: {
       outDir: 'dist',
